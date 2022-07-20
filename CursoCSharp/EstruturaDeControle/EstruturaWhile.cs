d5d186c76526de407bb62f2dbdf2139e11ba95b8
Bruno Haiek <bruno.haiek02@gmail.com>
2022-07-20 19:39:26 -0300

    Estrutura While

--- CursoCSharp/EstruturaDeControle/EstruturaWhile.cs
+++ CursoCSharp/EstruturaDeControle/EstruturaWhile.cs
@@ -14,6 +14,36 @@ namespace CursoCSharp.EstruturaDeControle
             Random random = new Random();
 
             int numeroSecreto = random.Next(1, 16); //Gerar um valor entre 1 e 15, o 16 não encaixa
+            bool numeroEncontrado = false;
+            int tentativasRestantes = 5;
+            int tentativasAtual = 0;
+
+
+            while (tentativasRestantes > 0 && !numeroEncontrado)
+            {
+                Console.Write("Insira seu palpite: ");
+                string entrada = Console.ReadLine();
+                int.TryParse(entrada, out palpite);
+
+                tentativasAtual++;
+                tentativasRestantes--;
+
+                if (numeroSecreto == palpite)
+                {
+                    numeroEncontrado = true;
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Parabéns você acertou o numero secreto em {0} Tentativas", tentativasAtual);
+                    Console.ResetColor();
+                }
+                else if (palpite > numeroSecreto)
+                {
+                    Console.WriteLine("Menor.. voce tem mais {0} Tentativas", tentativasRestantes);
+                }
+                else if (palpite < numeroSecreto)
+                {
+                    Console.WriteLine("Maior.. voce tem mais {0} Tentativas", tentativasRestantes);
+                }
+            }
         }
     }
 }
